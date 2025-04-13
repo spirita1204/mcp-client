e43a2b65f8c14703e2718257d7c5b4e31f2743ce
7141 陳定宏 danielchen <11203002@pershing.com.tw>
2025-04-14 00:23:02 +0800

    紀錄目標位置 db

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, HTTPException
 import uvicorn
 from client import MCPManager
 from contextlib import asynccontextmanager
+import json
 
 manager = None
 
@@ -31,6 +32,7 @@ app = FastAPI(lifespan=lifespan)
 async def process_message(request: dict):
     try:
         message = request.get("message")
+
         session_id = request.get("session_id")
         if not message:
             raise HTTPException(status_code=400, detail="Message is required")
@@ -42,5 +44,30 @@ async def process_message(request: dict):
         traceback.print_exc()
         raise HTTPException(status_code=500, detail=str(e))
 
+"""初始化寫入終點資訊"""
+@app.post("/api/")
+async def insert_endPoint(request: dict):
+    try:
+        prompt  = generate_prompt(request)
+        session_id = request.get("session_id")
+        print(f"prompt:{prompt}")
+        response = await manager.process_message(prompt, session_id)
+        return {"response": response}
+    except Exception as e:
+        import traceback
+        traceback.print_exc()
+        raise HTTPException(status_code=500, detail=str(e))
+
+"""產生對應prompt"""
+def generate_prompt(data):
+    match data.get("name"):
+        case "endpoint":
+            return f"幫我將該筆 json 資訊 {escape_quotes_in_data(data)} insert 到 EndPoint table 中"
+        
+"""處理字典中的所有字符串，將雙引號加上反斜線。"""
+def escape_quotes_in_data(data):
+    # 使用 json.dumps() 將字典轉換為帶有反斜線的 JSON 字符串
+    return json.dumps(data)
+    
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
